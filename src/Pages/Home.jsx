a6cd88a76ca82302669253cb93c3dc14ba569e6e
carloblvd <carloblvd@gmail.com>
2023-09-20 04:12:13 -0400

    first commit, start of project

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Landing from "../components/Home/Landing";
+import Features from "../components/Home/Features";
+import Reviews from "../components/Home/Reviews";
+import Numbers from "../components/Home/Numbers";
+
+const Home = () => {
+  return (
+    <div>
+      <Landing />
+      <Features />
+      <Reviews />
+      <Numbers />
+    </div>
+  );
+};
+
+export default Home;
